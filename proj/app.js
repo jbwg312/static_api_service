b845f00bb81e8ad9ede48945423222c257e4fcc9
Jerry Benton Walden Goebel <jbwg312@Jerrys-MacBook-Pro.local>
2016-07-01 10:56:43 -0600

    Initial Commit Static APi

---
+++ proj/app.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "http://localhost:3000/users",
+    "method": "GET",
+    "headers": {
+      "cache-control": "no-cache",
+      "postman-token": "29b15c9c-7e80-747a-8b39-8889472b7c0a"
+    }
+  }
+
+
+  $.ajax(settings).done(function (response) {
+
+    $(response).each(function(i, val){
+      $('#kyle').append(`<a href="http://localhost:3000/users/${val.user_data.id}"><li>${val.user_data.first_name} ${val.user_data.last_name}</li></a>`);
+    })
+    $(response).each(function(i, val){
+      console.log(val);
+      if(val.user_data.active === 1){
+        $('#maiman').append(`<a href="http://localhost:3000/users/${val.user_data.id}"><li>${val.user_data.first_name} ${val.user_data.last_name}</li></a>`);
+      }
+    })
+  });
+})
---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var data = require('../lib/data');
+
+
+/* GET home page. */
+router.get('/users', function(req, res, next) {
+  res.json(data);
+});
+router.get('/active_users', function(req, res, next) {
+  res.json(data);
+});
+router.get('/users/:id', function(req, res, next) {
+  res.render("index", {title: "Kyle Maiman", ben: req.params.id})
+});
+
+module.exports = router;
